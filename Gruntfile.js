8d941e342b425b823aa60792b0d42b8bc16f35ab
Pierre GIRAUD <pierre.giraud@dalibo.com>
2017-11-24 11:51:29 +0100

    Install packages using Yarn
    With this we get rid of bower to install packages
    Also grunt script are simplified (no need for a copy of css to scss). Sass can
    already include regular css files.

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,18 +12,6 @@ module.exports = function(grunt) {
       }
     },
     copy: {
-        cssAsScss: {
-            files: [
-            {
-                expand: true,
-                cwd: 'powa/static/',
-                src: ['bower_components/**/*.css', 'libs/**/*.css', '!**/*.min.css'],
-                dest: 'powa/static/',
-                filter: 'isFile',
-                ext: ".scss"
-            }
-            ]
-        },
         requirejs: {
             files: [
             {
@@ -59,19 +47,6 @@ module.exports = function(grunt) {
                 'powa/static/css/powa-all.min.css': 'powa/static/scss/powa.scss'
             }
         }
-    },
-
-    bowerRequirejs: {
-      all: {
-        options: {
-          baseUrl: "",
-          excludeDev: true,
-          transitive: true
-        }
-      },
-      target: {
-        rjsConfig: 'powa/static/js/config.js'
-      }
     }
   });
 
@@ -79,13 +54,12 @@ module.exports = function(grunt) {
   // Load the plugin that provides the "uglify" task.
 
   // Default task(s).
-  grunt.loadNpmTasks('grunt-bower-requirejs');
   grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-requirejs');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-sass');
 
-  grunt.registerTask('default', ['bowerRequirejs', 'copy', 'sass']);
-  grunt.registerTask('dist', ['bowerRequirejs', 'copy', 'requirejs', 'sass']);
+  grunt.registerTask('default', ['copy', 'sass']);
+  grunt.registerTask('dist', ['copy', 'requirejs', 'sass']);
 
 };
