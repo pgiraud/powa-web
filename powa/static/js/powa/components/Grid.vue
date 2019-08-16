88e8af5543ed9872fcbbb053eede20c285f4c11e
Pierre GIRAUD <pierre.giraud@dalibo.com>
2019-08-16 17:23:23 +0200

    Getting started with first vue components

---
+++ powa/static/js/powa/components/Grid.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h4>{{ config.title }}</h4>
+    I'm a grid
+  </div>
+</template>
+
+<script>
+
+import Widget from './Widget.vue';
+
+export default {
+  extends: Widget,
+  props: {
+    config: {
+      type: Object,
+      default () {return {}}
+    }
+  }
+}
+</script>
---
+++ powa/static/js/powa/components/Widget.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h4>{{ config.title }}</h4>
+  </div>
+</template>
+
+<script>
+
+export default {
+  props: {
+    config: {
+      type: Object,
+      default () {return {}}
+    }
+  },
+  methods: {
+    widgetComponent (type) {
+      return type;
+    }
+  }
+}
+</script>
