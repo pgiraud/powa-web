7f600a60d4dd7def35670b29901abfa926a4a539
Pierre GIRAUD <pierre.giraud@dalibo.com>
2019-09-02 08:00:34 +0200

    Foundation -> Bootstrap

--- powa/static/js/powa/main_entry.js
+++ powa/static/js/powa/main_entry.js
@@ -7,6 +7,8 @@ import BootstrapVue from 'bootstrap-vue';
 
 Vue.use(BootstrapVue);
 
+import 'bootstrap';
+
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
