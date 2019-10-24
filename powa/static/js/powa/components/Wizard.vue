eb5bc5729e116e60bbd9b55edbc2b4a8fdf32507
Pierre GIRAUD <pierre.giraud@dalibo.com>
2019-10-24 16:19:10 +0200

    Prepare wizard component

---
+++ powa/static/js/powa/components/Wizard.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="card mb-4">
+    <div class="card-body">
+      <h4>{{ config.title }}</h4>
+      <em>Wizard component not implemented.</em>
+    </div>
+  </div>
+</template>
+
+<script>
+import Component from 'vue-class-component'
+import Widget from './Widget.vue';
+
+@Component()
+class Wizard extends Widget {
+}
+
+export default Wizard
+</script>
