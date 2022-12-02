7c5b5235003f29a1645852a580bddb9bd8287790
Pierre GIRAUD <pierre.giraud@dalibo.com>
2022-12-02 19:00:05 +0100

    Avoid duplicated title in Content component

--- powa/query.py
+++ powa/query.py
@@ -347,7 +347,7 @@ class QueryExplains(ContentWidget):
     """
     Content widget showing explain plans for various const values.
     """
-    title = "Query Explains"
+    title = "Example Values"
     data_url = r"/server/(\d+)/metrics/database/([^\/]+)/query/(-?\d+)/explains"
 
     def get(self, server, database, query):
@@ -544,7 +544,7 @@ class QueryDetail(ContentWidget):
     """
     Detail widget showing summarized information for the query.
     """
-    title = "Query Detail"
+    title = "Index Suggestions"
     data_url = r"/server/(\d+)/metrics/database/([^\/]+)/query/(-?\d+)/detail"
 
     def get(self, srvid, database, query):
