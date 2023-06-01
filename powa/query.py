6f93b06ca39f04d266661f5a820031ca57659c24
Pierre-Louis GONON <pierre-louis.gonon@dalibo.com>
2023-06-01 11:32:50 +0200

    Revert title modification made in #9fea7bb

--- powa/query.py
+++ powa/query.py
@@ -353,7 +353,7 @@ class QueryExplains(ContentWidget):
     """
     Content widget showing explain plans for various const values.
     """
-    title = "Example Values"
+    title = "Query Explains"
     data_url = r"/server/(\d+)/metrics/database/([^\/]+)/query/(-?\d+)/explains"
 
     def get(self, server, database, query):
@@ -539,7 +539,7 @@ class QueryDetail(ContentWidget):
     """
     Detail widget showing summarized information for the query.
     """
-    title = "Index Suggestions"
+    title = "Query Detail"
     data_url = r"/server/(\d+)/metrics/database/([^\/]+)/query/(-?\d+)/detail"
 
     def get(self, srvid, database, query):
