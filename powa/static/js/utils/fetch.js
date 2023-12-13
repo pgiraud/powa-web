7429b56826e1dbd744adecc140d13b0bb27e22e2
Pierre GIRAUD <pierre.giraud@dalibo.com>
2023-12-13 12:24:44 +0100

    Rename name to metric in useFetch

--- powa/static/js/utils/fetch.js
+++ powa/static/js/utils/fetch.js
@@ -1,7 +1,7 @@
 import { onMounted, ref, watch } from "vue";
 import { useStoreService } from "@/composables/useStoreService";
 
-export function useFetch(name) {
+export function useFetch(metric) {
   const loading = ref(false);
   const data = ref(undefined);
   const { dataSources } = useStoreService();
@@ -18,7 +18,7 @@ export function useFetch(name) {
 
   function loadData() {
     loading.value = true;
-    const sourceConfig = dataSources.value[name];
+    const sourceConfig = dataSources.value[metric];
     sourceConfig.promise.then((response) => {
       data.value = JSON.parse(response);
       loading.value = false;
